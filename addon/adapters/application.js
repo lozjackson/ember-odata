8553a5472a36535f1bdc13e6779dbcc337d5d072
Loz Jackson <lozjackson@talktalk.net>
2016-02-14 21:53:47 +0000

    add application adapter

---
+++ addon/adapters/application.js
@@ -0,0 +1,11 @@
+/**
+  @module ember-odata
+*/
+import OdataAdapter from 'ember-odata/adapters/odata';
+
+/**
+  @class ApplicationAdapter
+  @extends EmberOdata.OdataAdapter
+  @namespace EmberOdata
+*/
+export default OdataAdapter.extend();
